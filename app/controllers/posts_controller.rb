fd198c4c7deb33be0be19f8bc9ca0dc1a8c9d987
Tyler Stewart <stewartt4@nku.edu>
2014-01-18 23:30:46 +0000

    Fix show posts error. Move out of private to public.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,13 +12,15 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
   
+  def index
+    @posts = Post.all
+  end
+  
+  
+  
   private
   def post_params
     params.require(:post).permit(:title, :text)
-  end
-  
-  def index
-    @posts = Post.all
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,6 @@ NkuRails::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'welcome#index'
-
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
