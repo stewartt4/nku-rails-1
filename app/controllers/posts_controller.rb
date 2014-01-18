49b8d8d2bd7d80b7d6928272601f844eed630ccd
Tyler Stewart <stewartt4@nku.edu>
2014-01-18 21:34:30 +0000

    Allow posts to be saved to db

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,8 @@ class PostsController < ApplicationController
   end
   
   def create
-    render text: params[:post].inspect
+    @post = Post.new(params[:post])
+    @post.save
+    redirect_to @post
   end
 end
