0f44921ff4cd3ade93d070f70afaf66ff6b292a2
Tyler Stewart <stewartt4@nku.edu>
2014-01-18 21:45:21 +0000

    permit post form submission

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,4 +7,9 @@ class PostsController < ApplicationController
     @post.save
     redirect_to @post
   end
+  
+  private
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
 end
