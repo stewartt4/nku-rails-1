aa597bbd58fe6405a9fb355a0bd5d1415c254d72
Tyler Stewart <stewartt4@nku.edu>
2014-01-19 00:42:29 +0000

    Validation in controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,15 @@
 class PostsController < ApplicationController
   def new
+    @post = Post.new
   end
   
   def create
-    @post = Post.new(post_params)
-    @post.save
-    redirect_to @post
+    @post = Post.new(params[:post].permit(:title, :text))
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
   end
     
   def show
