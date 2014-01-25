112c941b24c3853e643eec30a1202dca05d2e7b6
Tyler Stewart <stewartt4@nku.edu>
2014-01-25 18:15:36 +0000

    Recreate database because deleted model but did not do rollback...full of fails today

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140125153400) do
+ActiveRecord::Schema.define(version: 20140125181247) do
 
   create_table "comments", force: true do |t|
     t.string   "commenter"
@@ -31,6 +31,10 @@ ActiveRecord::Schema.define(version: 20140125153400) do
   end
 
   create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "nickname"
+    t.string   "email"
+    t.string   "image"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
