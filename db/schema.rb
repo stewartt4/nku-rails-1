218c4ba807c1df0ac3dc35ac47e0f9bca2f2a87b
Tyler Stewart <stewartt4@nku.edu>
2014-01-25 15:52:33 +0000

    user db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140119012304) do
+ActiveRecord::Schema.define(version: 20140125153400) do
 
   create_table "comments", force: true do |t|
     t.string   "commenter"
@@ -26,6 +26,11 @@ ActiveRecord::Schema.define(version: 20140119012304) do
   create_table "posts", force: true do |t|
     t.string   "title"
     t.text     "text"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
   end
