a1105f737cf1314d743db5223737f443d71239b0
Tyler Stewart <stewartt4@nku.edu>
2014-01-28 20:11:48 +0000

    Add email regex to catch invalid emails

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,10 @@ class User < ActiveRecord::Base
   include Gravtastic
   gravtastic :default => :identicon
   
+  VALID_EMAIL = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  
   validates :name, presence: true, uniqueness: true
-  validates :email, presence: true, uniqueness: true { case_sensitive: false }
+  validates :email, presence: true, format: { with: VALID_EMAIL }, 
+  uniqueness: { case_sensitive: false }
   validates :nickname, presence: true
 end
