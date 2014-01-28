7bdd367d7bfe7e879db9e7d8525c8b9b01cc696e
Tyler Stewart <stewartt4@nku.edu>
2014-01-28 20:06:19 +0000

    add some form validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
   include Gravtastic
   gravtastic :default => :identicon
+  
+  validates :name, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true { case_sensitive: false }
+  validates :nickname, presence: true
 end
