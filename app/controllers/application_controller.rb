c9b7d6ee606189b2f41dd4fbcce75289e6417a72
Tyler Stewart <stewartt4@nku.edu>
2014-02-01 21:32:54 +0000

    fix signin issue and show currently signed in user.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   #force_ssl
   include SessionsHelper
-
-private
- def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  end
-  helper_method :current_user
+  
+#private
+# def current_user
+#    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+#  end
+#  helper_method :current_user
 
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -11,4 +11,8 @@ module SessionsHelper
   def current_user
     @current_user ||= User.find_by_remember_token(cookies[:remember_token])
   end
+  
+  def signed_in? 
+    !current_user.nil?
+  end
 end
