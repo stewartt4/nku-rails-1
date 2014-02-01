745b4871eb8a9631344d40ecb81a7bee5d91db99
Tyler Stewart <stewartt4@nku.edu>
2014-02-01 15:09:13 +0000

    Match routes for signup, signin and signout

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,11 @@ NkuRails::Application.routes.draw do
   end
   resources :users
   resources :sessions
+  
+  # For sessions controller
+  match '/signup', to: 'users#new'
+  match '/signin', to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
