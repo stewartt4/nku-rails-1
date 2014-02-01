88663995a1bd8575ba5ee71fb4a95a7865ee7421
Tyler Stewart <stewartt4@nku.edu>
2014-02-01 15:05:31 +0000

    add session resources to routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ NkuRails::Application.routes.draw do
     resources :comments
   end
   resources :users
+  resources :sessions
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
