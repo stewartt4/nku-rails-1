f20d4fb30146270fc4a7e5b8884e9bfe0e39d9cf
Tyler Stewart <stewartt4@nku.edu>
2014-02-14 14:46:05 +0000

    add attended_on functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,24 +21,18 @@ class UsersController < ApplicationController
   
   def indexAttendances
     @users = User.all
-    #if current_user
-    #  current_user[:attendance] = true
-    #  current_user.save
-    #  signin(@user)
-    #else
-    #  redirect_to root_path, notice: "You need to be signed in to do that."
-    #end
   end
   
   def editAttendances
-    @user = current_user.id
   end
   
   
   def update
     @user = User.find(params[:id])
-    current_user[:attendance] = true  
-    if @user.update(params[:user].permit(:name, :nickname, :email, :image, :seat_num))
+    current_user[:attendance] = true
+    current_user[:attended_on] = Time.now
+    current_user.save
+    if @user.update(params[:user].permit(:name, :nickname, :email, :image, :seat_num, :attended_on))
       sign_in(@user)
       redirect_to root_path, notice: "User successfully changed!"
     else
