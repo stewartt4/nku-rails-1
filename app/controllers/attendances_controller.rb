7ad0e6e0cc4fa58fb8a84ab6330c5dabab9bfa10
Tyler Stewart <stewartt4@nku.edu>
2014-02-22 20:29:09 +0000

    allow only one attendance per day

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -5,29 +5,45 @@ class AttendancesController < ApplicationController
   
   def create
     @attendance = Attendance.new(params[:attendance].permit(:seat_num))
+    @attendance.id = current_user.id
     @attendance.student_name = current_user.name
     @attendance.attended_on = Date.today
     @attendance.expire = @attendance.attended_on + 1.days
-    if (@attendance.attended_on >= @attendance.expire) or (@attendance.attended_on == nil)
-      @attendance.save
+    current_user.attended_on = @attendance.attended_on
+    current_user.save
+    if @attendance.save
       redirect_to attendances_path, notice: "Attendance successfully logged!"
     else
       render 'new'
     end
   end
   
-  #def update
- #   @attendance = Attendance.find(params[:id])
- #   @attendance[:attendance] = true
- #   @attendance[:attended_on] = Time.now
- #   if (@attendance.attended_on >= @attendance.expire) or (@attendance == nil)
- #     @attendance.save
- #     sign_in(@attendance)
- #     redirect_to attendances_path, notice: "Attendance Successfully logged!"
- #   else
- #     render 'edit'
- #   end
- # end
+  def edit
+    @attendance = Attendance.find(params[:id])
+      #@attendance.student_name = current_user.name
+      #@attendance.attended_on = Date.today
+      #@attendance.expire = @attendance.attended_on + 1.days
+      #@attendance.save
+      #redirect_to attendances_path, notice: "Attendance successfully logged!"
+    #else
+    #  render 'edit'
+    #end
+  end
+  
+   def update
+     @attendance = Attendance.find(params[:id])
+    
+     if @attendance.attended_on >= @attendance.expire
+       @attendance.attended_on = Time.now
+       @attendance.expire = @attendance.attended_on + 1.days
+    
+       if @attendance.update(params[:user].permit(:seat_num))
+         redirect_to attendances_path, notice: "Attendance successfully changed!"
+       end
+    else
+       redirect_to edit_attendance_path, notice: "You are already here for today!"
+    end
+  end
   
   def index
     @attendances = Attendance.all
