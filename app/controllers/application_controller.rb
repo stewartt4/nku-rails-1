11c55338368c78329a04ceeaa716a19be9265536
Tyler Stewart <stewartt4@nku.edu>
2014-03-07 03:16:07 +0000

    add .vimrc file to display line numbers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,14 +9,20 @@ class ApplicationController < ActionController::Base
   end
   helper_method :current_user
   
+  def logged_in?
+    current_user != nil
+  end
+  
   before_action :require_login
-  skip_before_action :require_login, only: [:signin, :new] 
+  
+  skip_before_action :require_login, only: [:new, :create]
+  
   private
  
   def require_login
-    unless current_user
-      redirect_to signin_path # halts request cycle
+    unless logged_in?
+      flash[:error] = "You must be logged in to access this section"
+      redirect_to signin_path
     end
   end
-
 end
