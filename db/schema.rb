5d9df7fd5d580802c19fa557ee52a8f3e04c98bb
Tyler Stewart <stewartt4@nku.edu>
2014-03-11 16:32:33 +0000

    create admin field in User

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140307211119) do
+ActiveRecord::Schema.define(version: 20140311162203) do
 
   create_table "assignments", force: true do |t|
     t.datetime "created_at"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20140307211119) do
     t.datetime "updated_at"
     t.string   "password_digest"
     t.string   "remember_token"
+    t.boolean  "admin",           default: false
   end
 
   add_index "users", ["remember_token"], name: "index_users_on_remember_token"
